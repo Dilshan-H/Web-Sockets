910758c56b696a7b4978c623b265813da6107a40
dilshan-h <77499497+Dilshan-H@users.noreply.github.com>
2022-07-11 15:52:28 +0530

    Update Client.py

---
+++ client.py
@@ -0,0 +1,23 @@
+import asyncio
+import websockets
+
+async def hello():
+    uri = "ws://localhost:8765"
+    async with websockets.connect(uri) as websocket:
+        print("-" * 25)
+        print("Hello! Welcome User...")
+        print("Select choice: R | P | S or Q to quit")
+        print("-" * 25)
+        while True:
+            choice = input("Your choice is...? ")
+            choice = choice.strip()
+            if choice.lower() == "q":
+                break
+            await websocket.send(choice)
+            result = await websocket.recv()
+            print(result)
+            print("-" * 25)
+        print("Thank You!")
+
+if __name__ == "__main__":
+    asyncio.run(hello())
